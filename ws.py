14df7aa00207039ff3f2b05567be11e9cf0934cb
Hamza MOTASSIM <motassimhamza99@gmail.com>
2023-12-20 16:58:34 +0100

    ws

--- ws.py
+++ ws.py
@@ -1,16 +1,18 @@
 
 from bs4 import BeautifulSoup
 import requests
-from dotenv import load_dotenv
-import os
+# from dotenv import load_dotenv
+# import os
 
 # ETL function
 def run_etl(link):
     try:
-        load_dotenv()
+        # load_dotenv()
 
         # add your user agent 
-        usrAgnt = os.getenv("USER_AGENT")
+        # usrAgnt = os.getenv("USER_AGENT")
+        usrAgnt = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 \
+            (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36"
         HEADERS = ({'User-Agent': usrAgnt, 'Accept-Language': 'en-US, en;q=0.5'})
 
         # HTTP Request
@@ -42,4 +44,4 @@ def run_etl(link):
     
 
 if __name__ == "__main__":
-    print(run_etl(""))
+    print(run_etl("https://www.hespress.com/%d8%ac%d8%af%d9%84-%d9a7%d9%84%d9%85%d9%88%d8%aa%d9%89-%d8%a8%d8%a7%d9%84%d8%ac%d8%af%d9%8a%d8%af%d8%a9-1283197.html"))
